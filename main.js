07dc6ac9cbe9139e69e44323353b3e457a5ed731
loay suwwan <loaysuwwan20>
2024-01-18 14:48:00 +0200

    task2_js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-document.querySelector("div").classList.add("demo");
-document.querySelector("h1").classList.add("vip");
-document.querySelector("h1").classList.remove("vip");
+
+var registerForm = document.querySelector('form.registerForm');
+var res = 0;
+
+registerForm.onsubmit = function(event) {
+    event.preventDefault();
+    var elements = event.target.elements;
+    var currency = elements['exchange'].value;
+    var amount = elements['amount'].value;
+
+    if (currency == 'USD') {
+        res = amount * 0.26; // Assuming 0.26 is the conversion rate from Shekels to USD
+        document.querySelector(".result").textContent = "Converted amount: " + res + " USD";
+    }
+    else if(currency == 'JOD'){
+        res = amount *0.19; // Assuming 0.26 is the conversion rate from Shekels to USD
+        document.querySelector(".result").textContent = "Converted amount: " + res + " JOD";
+
+    }else{
+        document.querySelector(".result").textContent = "Converted amount: " + amount + " ILS";
+
+
+    }
+}
